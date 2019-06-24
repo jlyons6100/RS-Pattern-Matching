340675a8ae9461b1ee8331802f4f877749ab78ed
James Lyons <james@MacBook-Air-369.local>
2019-06-24 00:00:21 -0400

    Cleaned up machine_learning_all

--- machine_learning_all.py
+++ machine_learning_all.py
@@ -21,7 +21,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-
+# Runescape item category names
 cat_names = ["Miscellaneous","Ammo", "Arrows", "Bolts", "Construction_Materials"
 , "Construction_Projects", "Cooking_Ingredients","Costumes"
 ,"Crafting_Materials","Familiars","Farming_Produce","Fletching_Materials"
@@ -31,134 +31,72 @@ cat_names = ["Miscellaneous","Ammo", "Arrows", "Bolts", "Construction_Materials"
 "Range_Armour","Range_Weapons","Runecrafting","Runes_Spells_Teleports","Seeds","Summoning_Scrolls",
 "Tools_and_Containers","Woodcutting_Products","Pocket_Items"]
 
-models = []
-models.append(('LogisticRegression', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LinearDiscriminantAnalysis', LinearDiscriminantAnalysis()))
-models.append(('KNeighborsClassifier', KNeighborsClassifier()))
-models.append(('DecisionTreeClassifier', DecisionTreeClassifier()))
-models.append(('GaussianNB', GaussianNB()))
-#models.append(('SVM w/ rbf kernel', SVC(gamma='auto', kernel='rbf')))
-
-m = 7 # Prices from the past m days
-n = 7 # Predict if price increases over the next n days. #Combined needs to factor of 180 
-n_splits = 10 # Number of k-fold splits, decrease this number if it doesn't work due to large m + n size
-
-def get_data_from_row(row):
-    global models
+def get_data_from_row(row, m, n):
     arr = list(row)
+    # NORMALIZING DATA FROM ARR
     minn = min(arr)
     for x in range(len(arr)):
         arr[x] = arr[x] - minn
     maxx = max(arr)
     for x in range(len(arr)):
         if arr[x] != 0:
-            arr[x] = maxx / arr[x] 
-    global m, n, n_splits
-    X = []
-    Y = []
+            arr[x] = maxx / arr[x]
+    # NORMALIZING DATA FROM ARR
+    row_X = []
+    row_Y = []
     true_set = set()
+    # Predict if price increases over the next n days based on the previous m
     for x in range(0, len(arr)-m-n, m+n+1):
         past_m = list(arr[x:x+m])
-        X.append(past_m)
-        next_n = True if arr[x+m] < arr[x+m+n] else False
-        Y.append(next_n)
+        row_X.append(past_m)
+        next_n = True if arr[x+m] < arr[x+m+n] else False # True if price increased over next n days
+        row_Y.append(next_n)
         true_set.add(next_n)
+    # Making sure all data points aren't in the same class, not useful for ML
     if len(true_set) == 1: 
         return [], []
-    return X, Y
-
-    # try:
-    #     #validation_size = 0.00
-    #     #seed = 7
-    #     #scoring = "accuracy"
-    #     #X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-        
-        
-    #     # if len(prev_X) == 0:
-    #     #     prev_X = X_train
-    #     #     prev_Y = Y_train
-    #     # else:
-    #     #     X_train.extend(prev_X)
-    #     #     Y_train.extend(prev_Y)
-    #     #     prev_X = X_train
-    #     #     prev_Y = Y_train
-    #     results = []
-    #     names = []
-        
-    #     for name, model in models:
-    #         print(name)
-    #         kfold = model_selection.KFold(n_splits=n_splits, random_state=seed)
-    #         cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    #         results.append(cv_results)
-    #         names.append(name)
-    #         if name not in values:
-    #             values[name] = [cv_results.mean()]
-    #         else:
-    #             values[name].append(cv_results.mean())
-    #         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    # except:
-    #     pass
-def test_models(X, Y, values):
-    global n_splits, models
-    #try:
-    if True:
-        validation_size = 0.00
-        seed = 7
-        scoring = "accuracy"
-        results = []
-        names = []
-        
-        for name, model in models:
-            #print(name)
-            kfold = model_selection.KFold(n_splits=n_splits, random_state=seed)
-            cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
-            results.append(cv_results)
-            names.append(name)
-            if name not in values:
-                values[name] = [cv_results.mean()]
-            else:
-                values[name].append(cv_results.mean())
-            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-            print(msg)
-    #except:
-        #pass
-def test_models_on_cat(model_averages, headers, cat):
-    #print("Category: "+ cat)
-    global prev_X, prev_Y
+    return row_X, row_Y
+def test_models(X, Y):
+    models = []
+    models.append(('LogisticRegression', LogisticRegression(solver='liblinear', multi_class='ovr')))
+    models.append(('LinearDiscriminantAnalysis', LinearDiscriminantAnalysis()))
+    models.append(('KNeighborsClassifier', KNeighborsClassifier()))
+    models.append(('DecisionTreeClassifier', DecisionTreeClassifier()))
+    models.append(('GaussianNB', GaussianNB()))
+    # SVM problems with more than 200,000 data points
+    n_splits = 10 # Number of k-fold splits, decrease this number if it doesn't work due to large m + n size
+    seed = 7
+    scoring = "accuracy"
+    results = []
+    names = []   
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=n_splits, random_state=seed)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+def get_cat_data(headers, cat, m, n):
     df = pd.read_csv("item_graphs/"+cat+".csv", names=headers, index_col = 0)
-    
-    X = []
-    Y = []
-    # Calculating ML for each item in a category
+    cat_X = []
+    cat_Y = []
     for index, row in df.iterrows():
-        new_X, new_Y = get_data_from_row(row)
-        X.extend(new_X)
-        Y.extend(new_Y)
-    return X, Y
-    # test_models(X, Y, model_averages)
-
-def machine_learning():
-    headers = []
-    headers.append("Name")
-    global m,n
-    for x in  range(-179, 1):
-        headers.append(x)
-    sum_of_avr = []
-    model_averages = {}
-    #print("Model Averages for EACH Category")
-    X = []
-    Y = []
+        row_X, row_Y = get_data_from_row(row, m, n)
+        cat_X.extend(row_X)
+        cat_Y.extend(row_Y)
+    return cat_X, cat_Y
+# machine_learning(m, n): Tests models for ML, price increase in next n days based on previous m days of data
+def machine_learning(m, n):
+    headers = ["Name"] + [x for x in range(-179, 1)]
+    all_X = []
+    all_Y = []
     for cat in cat_names:
-        print("Model Averages for "+cat+" Category: (" +str(m) + ","+str(n)+")")
-        nX, nY = test_models_on_cat(model_averages, headers, cat)
-        X.extend(nX)
-        Y.extend(nY)
-    test_models(X, Y, model_averages)
-    #print(sum(sum_of_avr)/len(sum_of_avr))
-    # print("Overall Averages: (" +str(m) + ","+str(n)+")")
-    # for model_name in model_averages:
-    #     print(model_name)
-    #     print(sum(model_averages[model_name])/len(model_averages[model_name]))
+        print("Data for "+cat+" Category: (" +str(m) + ","+str(n)+")")
+        cat_X, cat_Y = get_cat_data(headers, cat, m, n)
+        all_X.extend(cat_X)
+        all_Y.extend(cat_Y)
+    print("Testing Models:")
+    test_models(all_X, all_Y)
 
-machine_learning()
-# python3 -W ignore machine_learning.py  to suppress warnings about collinear variables   
+machine_learning(7,7) 
+# Run with python3 -W ignore machine_learning.py  to suppress warnings about collinear variables   
